a659dbeed70b6666aab0e400691010e7b41059d8
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-09-01 20:10:39 +0300

    created repo

--- src/main/java/com/olegstashkiv/booksstore/BooksStoreApplication.java
+++ src/main/java/com/olegstashkiv/booksstore/BooksStoreApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BooksStoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BooksStoreApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BooksStoreApplication.class, args);
+    }
 }
