9de42b6d42c450546fadc1b9e6a4b11341ffb36e
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-09-13 15:38:51 +0300

    fixed checkstyle

--- src/main/java/com/olegstashkiv/booksstore/repository/shopingcart/ShoppingCartRepository.java
+++ src/main/java/com/olegstashkiv/booksstore/repository/shopingcart/ShoppingCartRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    Optional<ShoppingCart> findByUserId (Long id);
+    Optional<ShoppingCart> findByUserId(Long id);
 }
